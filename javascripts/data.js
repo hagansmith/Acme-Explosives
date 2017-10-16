0f0de7578f437888f858ee3a9d3f77878e843b97
Adam Smith <hagansmith@gmail.com>
2017-10-15 20:31:41 -0500

    added promise filtering and array work

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,13 +1,14 @@
 "use strict";
 
 var dom = require('./dom');
-
 var fireworks = [];
 
+
+// --- PROMISES --- //
 var categories = function(){
 	return new Promise(function(resolve, reject){
 		$.ajax('./data/categories.json').done(function(data1){
-			resolve(data1.categories);
+			resolve(data1);
       categoriesDom(data1.categories);
 		}).fail(function(error1){
 			reject(error1);
@@ -18,7 +19,7 @@ var categories = function(){
 var products = function(){
 	return new Promise(function(resolve, reject){
 		$.ajax('./data/products.json').done(function(data2){
-			resolve(data2.products);
+			resolve(data2);
 		}).fail(function(error2){
 			reject(error2);
 		});
@@ -28,38 +29,76 @@ var products = function(){
 var types = function(){
 	return new Promise(function(resolve, reject){
 		$.ajax('./data/types.json').done(function(data3){
-			resolve(data3.types);
+			resolve(data3);
 		}).fail(function(error3){
 			reject(error3);
 		});
 	});
 };
 
-var fireworksGetter = function(){
-	Promise.all([categories(), products(), types()]).then(function(results){
-		results.forEach(function(result){
-      result.forEach(function(firework){
-        fireworks.push(firework);
+// --- Promise Calls --- //
+var categoriesGetter = function(){
+	categories();
+};
+
+// Use the selection to filter results and initiate data call
+const dataFilter = (input) => {
+	Promise.all([categories(), types(), products()]).then((results) => {
+		results[0].categories.forEach(function(category){
+			//if (category.name === input) {
+			results[1].types.forEach(function(type){
+				if (category.id === type.category) {
+					type.catName = category.name;}
+					console.log(type);
+				results[2].products.forEach(function(product){
+					console.log(product);
+					//product.forEach(function(item){
+					//console.log(item);
+
+					if (product.type === type.id) {
+						product.typeName = type.name;
+						product.category = type.catName;
+					}
+						//console.log(product);
+					//});
+				});
 			});
 		});
-		//makeBigBoom();
-	}).catch(function(error){
-		console.log("error from Promise.all", error);
 	});
 };
 
+
+// 	let productArray=[];
+// 	categories().then(function(result){
+// 		 result.forEach(function(category){
+// 			 if (category.name === input) {
+// 				 productArray.push(category);
+// 			 }
+// 		});
+// 	}).then(types().then(function(productArray){
+// 		console.log('types',types);
+// 	}));
+// 	console.log('final product array', productArray);
+// };
+	//call write to dom table
+
+// --- DOM Functions --- //
 var categoriesDom = function(thing){
 	thing.forEach(function(cat){
 		dom(cat);
 	});
 };
 
+// Write to Dom table
+	//write the products to the dom
+
+// --- Master Functions --- //
 var initializer = function(){
-	fireworksGetter();
+	categoriesGetter();
 };
 
 var getFireworks = function(){
 	return fireworks;
 };
 
-module.exports = {initializer, getFireworks};
+module.exports = {initializer, getFireworks, dataFilter};
--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,15 +1,19 @@
 "use strict";
-
+// --- Write the categories to the DOM selector --- //
 var categoryDiv = $('#category-selector');
 
 var categorySelect = function(category) {
   var domString = '';
-      domString +=   `<option value=${category.name}>${category.name}</option>`;
+      domString +=   `<option value="${category.name}">${category.name}</option>`;
 	printToDom(domString);
 };
 
 var printToDom = function(string) {
 	categoryDiv.append(string);
 };
+
+// --- Write the selected products and their info to the DOM --- //
+
+
 
 module.exports = categorySelect;
